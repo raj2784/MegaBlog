63346af96c76841d1be2b3aaf23d996192f00a20
Raj Bhatia <raj2784@hotmail.com>
2024-01-31 21:20:51 +1030

    configure store with @reduxjstoolkit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
 
 function App() {
-  console.log(process.env.REACT_APP_APPWRITE_URL);
+  const [loading, setLoading] = useState();
+  const dispatch = useDispatch();
+  //console.log(process.env.REACT_APP_APPWRITE_URL);
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {})
+      .catch(() => {});
+  }, []);
 
   return (
     <h1 className="text-3xl font-bold underline text-white text-center">
---
+++ src/store/store.js
@@ -0,0 +1,8 @@
+import react from "react";
+import { configureStore } from "@reduxjs/toolkit";
+
+const store = configureStore({
+  reducer: {},
+});
+
+export default store;
