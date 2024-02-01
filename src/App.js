9acd6708ec16668fea9a8fda61e4f1a243b258d2
Rajan <rajan.bhatia@sa.gov.au>
2024-02-01 15:08:48 +1030

    updated 01022024

--- src/App.js
+++ src/App.js
@@ -2,24 +2,39 @@ import "./App.css";
 import { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import { Header, Footer } from "./components";
 
 function App() {
-  const [loading, setLoading] = useState();
+  const [loading, setLoading] = useState(true);
   const dispatch = useDispatch();
   //console.log(process.env.REACT_APP_APPWRITE_URL);
 
   useEffect(() => {
     authService
       .getCurrentUser()
-      .then((userData) => {})
-      .catch(() => {});
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .catch((error) => {})
+      .finally(() => {
+        setLoading(false);
+      });
   }, []);
 
-  return (
-    <h1 className="text-3xl font-bold underline text-white text-center">
-      Mega Blog!
-    </h1>
-  );
+  return !loading ? (
+    <div className="min-h-sc flex flex-wrap content-between bg-gray-500">
+      <div className="w-full block">
+        <Header />
+        <main>{/* <Outlet/> */}</main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
 export default App;
